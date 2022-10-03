a43408eed9574344ab92dbd234d97a939c76bf5e
Shreyaar12 <shreyarajpal6@gmail.com>
2022-10-03 23:20:32 +0530

    updates added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from "react";
 import AllRoutes from "./Routes/AllRoutes";
 import Navbar from "./Homepage/Homepage/Navbar";
-import "./try.css";
+
 function App() {
   return (
-    <div className="BIGB">
+    <div>
       <Navbar />
       <AllRoutes />
     </div>
