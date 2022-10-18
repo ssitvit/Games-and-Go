b5e7218a2538a600424041a80591e803b8f5d6bb
Gunjan Raj Tiwari <im.gunjan1@gmail.com>
2022-10-18 20:51:00 +0545

    99 game added

--- src/Routes/AllRoutes.js
+++ src/Routes/AllRoutes.js
@@ -1,12 +1,14 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Homepage from "../Homepage/Homepage/Homepage";
 import Tictactoe from "../Games/Tictactoe/tictactoe";
+import Ninetynine from "../Games/Ninetynine/ninetynine";
 function AllRoutes(){
     return (
         <Routes>
           {/* Add all the routes with the right path here after importing them  */}
         <Route path="/" element={<Homepage />} />
         <Route path="/Tic" element={<Tictactoe />} />
+        <Route path="/99" element={<Ninetynine />} />
       </Routes>
     )
 }
