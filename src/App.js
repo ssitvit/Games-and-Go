10df6d40d6cfac3a013ad3bad11f8a5c7f556732
Manisha Swain <manishaswain2003@gmail.com>
2023-05-24 11:38:08 +0530

    FREATURE added -simple the better  loader page issue-167

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import AllRoutes from "./Routes/AllRoutes";
 import Navbar from "./Homepage/Homepage/Navbar";
-
+import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
+import PacmanLoader from "react-spinners/PacmanLoader";
+import "./App.css";
 function App() {
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+  }, []);
   return (
     <div>
-      <Navbar />
-      <AllRoutes />
+      {loading ? (
+        <PacmanLoader
+          color={"#f1e702"}
+          loading={loading}
+          size={30}
+          aria-label="Loading Spinner"
+          data-testid="loader"
+          className="loader"
+        />
+      ) : (
+        <div>
+          <Navbar />
+          <AllRoutes />
+        </div>
+      )}
     </div>
   );
 }
