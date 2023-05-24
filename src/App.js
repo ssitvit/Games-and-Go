59590dbddfd0224aa133f2099f513fec3bc0657d
Manisha Swain <manishaswain2003@gmail.com>
2023-05-24 14:17:36 +0530

    FREATURE added -new  loader page issue-167

--- src/App.js
+++ src/App.js
@@ -1,12 +1,38 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import AllRoutes from "./Routes/AllRoutes";
 import Navbar from "./Homepage/Homepage/Navbar";
-
+import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
+import PacmanLoader from "react-spinners/PacmanLoader";
+import "./App.css";
 function App() {
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+  }, []);
   return (
     <div>
-      <Navbar />
-      <AllRoutes />
+      {loading ? (
+        <div>
+          <PacmanLoader
+            color={"#f1e702"}
+            loading={loading}
+            size={45}
+            aria-label="Loading Spinner"
+            data-testid="loader"
+            className="loader"
+          />
+          <p className="loader1">Games and go!!!</p>
+        </div>
+      ) : (
+        // (<p>Games-and-Go</p>)
+        <div>
+          <Navbar />
+          <AllRoutes />
+        </div>
+      )}
     </div>
   );
 }
