bbef3408c20a5961b01f14654a32892abe074c5c
KeyaDesai <121417626+Keya17Desai@users.noreply.github.com>
2023-05-29 10:15:47 +0530

    Add files via upload

--- src/Routes/AllRoutes.js
+++ src/Routes/AllRoutes.js
@@ -4,14 +4,8 @@ import Tictactoe from "../Games/Tictactoe/tictactoe";
 import Ninetynine from "../Games/Ninetynine/ninetynine";
 import Memory from "../Games/Memory/memory";
 import TriviaGame from "../Games/Trivia/quiz";
-import Simonsays from "../Games/Simonsays/simon";
 import Fifteenpuzzle from "../Games/15Puzzle/Fifteenpuzzle";
-import ClumsyBird from "../Games/ClumsyBird/clumsybird";
-import RockPaperScissors from "../Games/RockPaperScissors/RockPaperScissors";
-import BrickBreakout from "../Games/BrickBreakout/BrickBreakout";
-import Typo from "../Games/Typo/Typo";
-import BallShooting from "../Games/BallShooting/BallShooting";
-
+import clumsybird from "../Games/clumsybird";
 
 function AllRoutes() {
   return (
@@ -23,14 +17,8 @@ function AllRoutes() {
       <Route path="/memory" element={<Memory />} />
       <Route path="/trivia" element={<TriviaGame />} />
       <Route path="15puzzle" element={<Fifteenpuzzle />} />
-      <Route path="/clumsybird" element={<ClumsyBird />} />
-      <Route path="/simon" element={<Simonsays />} />
-      <Route path="/rock-paper-scissors" element={<RockPaperScissors />} />
-      <Route path="/brick-breakout" element={<BrickBreakout />} />
-      <Route path="/typo" element={<Typo />} />
-      <Route path="/BallShooting" element={<BallShooting/>} />
+      <Route path="/clumsybird" element={<clumsybird />} />
     </Routes>
   );
 }
-
 export default AllRoutes;
