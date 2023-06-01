96f7285cba16c3cdcc45303aab6157a5004451fd
Ishita <ishitasrivastava.salwan@gmail.com>
2023-06-01 20:30:29 +0530

    AllRoutes changed

--- src/Routes/AllRoutes.js
+++ src/Routes/AllRoutes.js
@@ -12,7 +12,6 @@ import Typo from "../Games/Typo/Typo";
 import BallShooting from "../Games/BallShooting/BallShooting";
 import MagicMatch from "../Games/Magic-match/magic_match";
 import Wordle from "../Games/Wordle/Wordle";
-import DiceThrow from "../Games/DiceThrow/main"
 
 function AllRoutes() {
   return (
@@ -31,7 +30,6 @@ function AllRoutes() {
       <Route path="/BallShooting" element={<BallShooting/>} />
       <Route path="/MagicMatch" element={<MagicMatch/>}/>
       <Route path="/Wordle" element={<Wordle/>}/>
-      <Route path="/DiceThrow" element={<DiceThrow/>}/>
     </Routes>
   );
 }
