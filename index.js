d8fe03b533c4cf653b0a9720192aa77a2667d31b
srushtikage <124689410+srushtikage@users.noreply.github.com>
2023-06-04 16:22:14 +0530

    The Dice Game
    By: Srushti Kage 
    GSSOC'23 Contributor

---
+++ index.js
@@ -0,0 +1,26 @@
+var randomNumber1= Math.floor(Math.random()*6)+1;
+
+var imageChange1= "images/dice"+randomNumber1+".png";
+
+document.querySelectorAll("img")[0].setAttribute("src", imageChange1);
+
+var randomNumber2= Math.floor(Math.random()*6)+1;
+
+var imageChange2= "images/dice"+randomNumber2+".png";
+
+document.querySelectorAll("img")[1].style.background;
+
+if(randomNumber1>randomNumber2)
+{
+    document.querySelector("h1").innerHTML="🚩Player 1 Wins!";
+}
+
+else if(randomNumber1<randomNumber2)
+{
+    document.querySelector("h1").innerHTML="Player 2 Wins!🚩";   
+}
+
+else
+{
+    document.querySelector("h1").innerHTML="Draw!";
+}
