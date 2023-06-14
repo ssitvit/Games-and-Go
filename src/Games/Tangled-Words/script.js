e9ba9cc28709d82e0284093a6bbb07a866f8e241
AnkitaBarbora <101138526+AnkitaBarbora@users.noreply.github.com>
2023-06-14 15:18:40 +0530

    Added the Tangled Words game

---
+++ src/Games/Tangled-Words/script.js
@@ -0,0 +1,47 @@
+const wordText = document.querySelector(".word"),
+hintText = document.querySelector(".hint span"),
+timeText = document.querySelector(".time b"),
+inputField = document.querySelector("input"),
+refreshBtn = document.querySelector(".refresh-word"),
+checkBtn = document.querySelector(".check-word");
+
+let correctWord, timer;
+
+const initTimer = maxTime => {
+    clearInterval(timer);
+    timer = setInterval(() => {
+        if(maxTime > 0) {
+            maxTime--;
+            return timeText.innerText = maxTime;
+        }
+        alert(`Time off! ${correctWord.toUpperCase()} was the word`);
+        initGame();
+    }, 1000);
+}
+
+const initGame = () => {
+    initTimer(50);
+    let randomObj = words[Math.floor(Math.random() * words.length)];
+    let wordArray = randomObj.word.split("");
+    for (let i = wordArray.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1));
+        [wordArray[i], wordArray[j]] = [wordArray[j], wordArray[i]];
+    }
+    wordText.innerText = wordArray.join("");
+    hintText.innerText = randomObj.hint;
+    correctWord = randomObj.word.toLowerCase();;
+    inputField.value = "";
+    inputField.setAttribute("maxlength", correctWord.length);
+}
+initGame();
+
+const checkWord = () => {
+    let userWord = inputField.value.toLowerCase();
+    if(!userWord) return alert("Please enter the word to check!");
+    if(userWord !== correctWord) return alert(`Nope~ ${userWord} is not the word`);
+    alert(`Yes! ${correctWord.toUpperCase()} is correct`);
+    initGame();
+}
+
+refreshBtn.addEventListener("click", initGame);
+checkBtn.addEventListener("click", checkWord);
