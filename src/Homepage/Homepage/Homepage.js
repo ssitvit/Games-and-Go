939f2d9b9033d1a8109283a3a806ca41a708d775
Swapnil Jadhav <89910173+Swapnil-2503@users.noreply.github.com>
2023-06-16 16:56:09 +0530

    Update Homepage.js for theme additions

--- src/Homepage/Homepage/Homepage.js
+++ src/Homepage/Homepage/Homepage.js
@@ -1,43 +1,59 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Homepage.css";
 import { Link } from "react-router-dom";
 import { data1 } from "../Data/data";
 import GoToTop from "./GoToTop";
 import MusicButton from "./MusicButton";
-import { useState } from "react";
+
+const themes = ["default", "white", "black", "red", "green"]; // Define the available themes
 
 function Homepage() {
-  const [searchQuery, setSearchQuery] = useState('');
+  const [searchQuery, setSearchQuery] = useState("");
   const [filteredData, setFilteredData] = useState(data1);
+  const [themeIndex, setThemeIndex] = useState(0); // Track the current theme index
 
   const handleSearch = (e) => {
     const inputValue = e.target.value;
     setSearchQuery(inputValue);
 
     const filtered =
-      inputValue === ''
+      inputValue === ""
         ? data1 // Show all values if search query is empty
         : data1.filter((item) =>
             item.main_heading.toLowerCase().includes(inputValue.toLowerCase())
           );
     setFilteredData(filtered);
   };
+
+  const changeTheme = () => {
+    setThemeIndex((prevIndex) => (prevIndex + 1) % themes.length); // Update the theme index
+  };
+
+  const theme = themes[themeIndex]; // Get the current theme based on the index
+
   return (
     <React.Fragment>
       <GoToTop />
       <MusicButton />
 
       {/* Pacman Landing Container */}
-      <div className="container_landing">
+      <div className={`container_landing ${theme}`}>
         <div className="pacman"></div>
-          <div className="ghost"></div>
-          <div className="ghost"></div>
-          <div className="ghost"></div>
-          <div className="ghost"></div>
-          <div className="text"></div>
+        <div className="ghost"></div>
+        <div className="ghost"></div>
+        <div className="ghost"></div>
+        <div className="ghost"></div>
+        <div className="text"></div>
       </div>
-      {/* Heading of Cards */}
-      <div className="header_homepage">
+ {/* Theme Selector */}
+ <div className="floating-button-container">
+        {/* Theme Selector */}
+        <button onClick={changeTheme} className={`floating-button ${theme}`}>
+          T
+        </button>
+      </div>
+     {/* Heading of Cards */}
+     <div className="header_homepage">
         <h1> Game on!!</h1>
       </div>
       {/* The content in the cards came from mapping data1, if you want to contribute a game kindly add it to data1 in the Data folder first*/}
