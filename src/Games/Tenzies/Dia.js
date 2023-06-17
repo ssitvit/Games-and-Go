3fc0f5ee5a16b4a86eca17399ad56880755fa5d7
Nishitbariya <85815172+Nishitbaria@users.noreply.github.com>
2023-06-17 19:40:13 +0530

    Big bug fix

--- src/Games/Tenzies/Dia.js
+++ src/Games/Tenzies/Dia.js
@@ -1,17 +1,12 @@
 import React from "react";
 
-export default function App(props) {
-const styles = {
-        backgroundColor: props.isHeld ? "skyblue" : "white"
-    }
-
-    return(
-    
-
-<div className="die-face" style={styles}    onClick={props.holdDice} >
-<h2 className="die-num">{props.value}</h2>
-</div>
-
-)
-
+export default function Die(props) {
+  const styles = {
+    backgroundColor: props.isHeld ? "#59E391" : "white",
+  };
+  return (
+    <div className="die-face" style={styles} onClick={props.holdDice}>
+      <h2 className="die-num">{props.value}</h2>
+    </div>
+  );
 }
