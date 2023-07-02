3580e222f2bc6c92370066cdc80b5a614ad5ec9d
ayush9951 <ayushgupta9951@gmail.com>
2023-07-02 15:13:05 +0530

    Improve card UI style

--- src/Homepage/Homepage/Homepage.js
+++ src/Homepage/Homepage/Homepage.js
@@ -7,7 +7,7 @@ import MusicButton from "./MusicButton";
 import { useState } from "react";
 
 function Homepage() {
-  const [searchQuery, setSearchQuery] = useState('');
+  const [searchQuery, setSearchQuery] = useState("");
   const [filteredData, setFilteredData] = useState(data1);
 
   const handleSearch = (e) => {
@@ -15,11 +15,11 @@ function Homepage() {
     setSearchQuery(inputValue);
 
     const filtered =
-      inputValue === ''
+      inputValue === ""
         ? data1 // Show all values if search query is empty
         : data1.filter((item) =>
-          item.main_heading.toLowerCase().includes(inputValue.toLowerCase())
-        );
+            item.main_heading.toLowerCase().includes(inputValue.toLowerCase())
+          );
     setFilteredData(filtered);
   };
   return (
@@ -38,7 +38,18 @@ function Homepage() {
       </div>
       <div className="about-section">
         <h2>About</h2>
-        <p>Games-And-Go is a website that offers a diverse collection of online games to entertain and engage users of all ages. With a user-friendly interface, the website provides easy access to a wide range of games, including puzzles, arcade classics, strategy games, and more. Users can explore different game categories, choose their favorites, and start playing instantly without the need for downloads or installations. The website aims to provide a fun and immersive gaming experience, allowing players to relax, challenge themselves, and enjoy their leisure time. Whether you're a casual gamer or a dedicated gaming enthusiast, Games-And-Go has something to offer for everyone.</p>
+        <p>
+          Games-And-Go is a website that offers a diverse collection of online
+          games to entertain and engage users of all ages. With a user-friendly
+          interface, the website provides easy access to a wide range of games,
+          including puzzles, arcade classics, strategy games, and more. Users
+          can explore different game categories, choose their favorites, and
+          start playing instantly without the need for downloads or
+          installations. The website aims to provide a fun and immersive gaming
+          experience, allowing players to relax, challenge themselves, and enjoy
+          their leisure time. Whether you're a casual gamer or a dedicated
+          gaming enthusiast, Games-And-Go has something to offer for everyone.
+        </p>
       </div>
       {/* Heading of Cards */}
       <div className="header_homepage">
@@ -56,32 +67,32 @@ function Homepage() {
         <div className="container_card">
           {filteredData.map((row) => (
             <div className="card" key={row.serial_number}>
-            <div className="content">
-              {/* Addition of Flip card feature */}
-              <div class="flip-card">
-                <div class="flip-card-inner">
-                  <div class="flip-card-front">
-                    <div>
-                      <h2>{row.serial_number}</h2>
-                      <h3>{row.main_heading}</h3>
-                      
+              <div className="content">
+                {/* Addition of Flip card feature */}
+                <div class="flip-card">
+                  <div class="flip-card-inner">
+                    <div class="flip-card-front">
+                      <div>
+                        <h2>{row.serial_number}</h2>
+                        <h3>{row.main_heading}</h3>
+                      </div>
                     </div>
-                    
-                  </div>
-                  <div class="flip-card-back">
-                    <div class="rule_heading">{row.rule_heading}</div>
-                    <div class="step">{row.step1}</div>
-                    <div class="step">{row.step2}</div>
-                    <div class="step">{row.step3}</div>
+                    <div class="flip-card-back">
+                      <div class="rule_heading">{row.rule_heading}</div>
+                      <div class="step">{row.step1}</div>
+                      <div class="step">{row.step2}</div>
+                      <div class="step">{row.step3}</div>
+                    </div>
                   </div>
                 </div>
+                {/* Flip card feature ends here. */}
+                <p className="about">{row.about} </p>
+                {/* Create a route for your game and add it in AllRoutes.js in Routes folder then add the link in data1 in Data Folder */}
+                <Link to={row.link_game} className="play-button">
+                  Play now !!!
+                </Link>
               </div>
-              {/* Flip card feature ends here. */}
-              <p>{row.about} </p>
-              {/* Create a route for your game and add it in AllRoutes.js in Routes folder then add the link in data1 in Data Folder */}
-              <Link to={row.link_game}>Play now !!!</Link>
             </div>
-          </div>
           ))}
         </div>
       </div>
