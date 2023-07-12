b87645d80af5f7b8ee98e9ad7cd7e53bd0496ba4
Jaivardhan Shukla <93859359+jaivsh@users.noreply.github.com>
2023-07-12 09:40:02 +0530

    Update data.js
    Added info for Pong_Game

--- src/Homepage/Data/data.js
+++ src/Homepage/Data/data.js
@@ -293,5 +293,17 @@ export const data1 = [
     step1: "1. Use forward and backward arrow to run the dino.",
     step2: "2. Use upward arrow to jump the dino over dragon to increase the scoreboard.",
     step3: "3. If dino comes in contact with dragon then game over.",
-  }
+  },
+  {
+    serial_number: "27",
+    main_heading: "Pong Game",
+    about: "Use paddles to drive the volley towards your opponent back and forth",
+    link_game: "/Pong_Game",
+    // Flip-card-back contents
+    rule_heading: "Rules to play",
+    step1: "1. Being a multiplayer game, the player 1 playes through arrow keys, while player 2 playes through secondary arrows(w-a-s-d)",
+    step2: "2. Both players try to score more and more and play consistently till one of them gives up.",
+    step3: "3. The one who survives till end wins.",
+  },
+  
 ];
