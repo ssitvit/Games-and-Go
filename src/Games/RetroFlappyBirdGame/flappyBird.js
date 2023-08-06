c028a32c9ebd8ed16bc24b1a8ae85ca5e00fec6f
jaibhedia <jaibhedia@gmail.com>
2023-08-06 14:08:46 +0530

    update flappybird.js

--- src/Games/RetroFlappyBirdGame/flappyBird.js
+++ src/Games/RetroFlappyBirdGame/flappyBird.js
@@ -3,7 +3,6 @@
 var cvs = document.getElementById("canvas");
 var ctx = cvs.getContext("2d");
 
-// load images
 var bird = new Image();
 var bg = new Image();
 var fg = new Image();
@@ -16,70 +15,75 @@ fg.src = "images/fg.png";
 pipeNorth.src = "images/pipeNorth.png";
 pipeSouth.src = "images/pipeSouth.png";
 
-// some variables
-var gap = 85;
+var gap = 140;
 var constant;
-var bX = 10;
-var bY = 150;
-var gravity = 1.5;
+var bX = 30;
+var bY = 175;
+var gravity = 1.0;
 var score = 0;
 
-// audio files
 var fly = new Audio();
 var scor = new Audio();
 
 fly.src = "sounds/fly.mp3";
 scor.src = "sounds/score.mp3";
 
-// on key down
-document.addEventListener("keydown", moveUp);
-
-function moveUp() {
-  bY -= 25;
-
-  fly.play();
-}
-
-// pipe coordinates
 var pipe = [];
-
 pipe[0] = {
   x: cvs.width,
-
   y: 0,
 };
 
-// draw images
+var gameOver = false; // Game over flag
+
+document.addEventListener("keydown", moveUp);
+
+function moveUp() {
+  if (!gameOver) { // Allow bird movement only when game is not over
+    bY -= 35;
+    fly.play();
+  }
+}
+
+function restartGame() {
+  gameOver = false;
+  score = 0;
+  pipe = [];
+  pipe[0] = {
+    x: cvs.width,
+    y: 0,
+  };
+  bY = 175;
+  draw();
+}
+
 function draw() {
   ctx.drawImage(bg, 0, 0);
 
   for (var i = 0; i < pipe.length; i++) {
     constant = pipeNorth.height + gap;
-
     ctx.drawImage(pipeNorth, pipe[i].x, pipe[i].y);
     ctx.drawImage(pipeSouth, pipe[i].x, pipe[i].y + constant);
-
     pipe[i].x--;
-
-    if (pipe[i].x == 125) {
+    
+    if (pipe[i].x == 130) {
       pipe.push({
         x: cvs.width,
         y: Math.floor(Math.random() * pipeNorth.height) - pipeNorth.height,
       });
     }
 
-    // detect collision
     if (
       (bX + bird.width >= pipe[i].x &&
-        bX <= pipe[i].x + pipeNorth.width &&
-        (bY <= pipe[i].y + pipeNorth.height ||
-          bY + bird.height >= pipe[i].y + constant)) ||
+      bX <= pipe[i].x + pipeNorth.width &&
+      (bY <= pipe[i].y + pipeNorth.height ||
+      bY + bird.height >= pipe[i].y + constant)) ||
       bY + bird.height >= cvs.height - fg.height
     ) {
-      location.reload(); // reload the page
+      gameOver = true;
     }
 
-    if (pipe[i].x == 5) {
+    if (pipe[i].x == 1) {
       score++;
       scor.play();
     }
@@ -87,12 +91,21 @@ function draw() {
 
   ctx.drawImage(fg, 0, cvs.height - fg.height);
   ctx.drawImage(bird, bX, bY);
-
   bY += gravity;
+
   ctx.fillStyle = "#000";
   ctx.font = "20px Verdana";
   ctx.fillText("Score : " + score, 10, cvs.height - 20);
-  requestAnimationFrame(draw);
+
+  if (gameOver) {
+    ctx.fillStyle = "#000";
+    ctx.font = "30px Verdana";
+    ctx.fillText("Game Over", cvs.width / 2 - 60, cvs.height / 2 - 30);
+    ctx.fillText("Click to Restart", cvs.width / 2 - 110, cvs.height / 2 + 10);
+    cvs.addEventListener("click", restartGame);
+  } else {
+    requestAnimationFrame(draw);
+  }
 }
 
 draw();
