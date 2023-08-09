b056c930bf2b3be73cb4ed17d382722c2ca31043
Simran <simran28singh28@gmail.com>
2023-08-09 23:56:00 +0530

    Code quality Improvements

--- src/ErrorPage/ErrorPage.js
+++ src/ErrorPage/ErrorPage.js
@@ -1,28 +1,29 @@
 import React from "react";
-import "./ErrorPage.css";
 import { NavLink } from "react-router-dom";
+import styled from "styled-components"; // You can use a CSS-in-JS library for styling
+
+const ErrorPageWrapper = styled.div`
+  /* Add your CSS styles here */
+`;
 
 const ErrorPage = () => {
   return (
-    <>
-      <div className="pg_not_found">
+    <ErrorPageWrapper>
+      <div className="page_not_found">
         <h1 className="error">
-          4
-          <span>
-            0
-          </span>
-          4
+          4<span>0</span>4
         </h1>
         <h2>Error: 404 page not found</h2>
         <p>Sorry, the page you're looking for cannot be accessed</p>
         <NavLink to="/">
-          <h3 class="btn">
-            <i class="bx bx-arrow-back bx-tada bx-flip-horizontal"></i> Go Back
+          <h3 className="btn">
+            <i className="bx bx-arrow-back bx-tada bx-flip-horizontal"></i> Go
+            Back
           </h3>
         </NavLink>
       </div>
-
-    </>
+    </ErrorPageWrapper>
   );
 };
+
 export default ErrorPage;
